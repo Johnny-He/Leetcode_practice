e512cba1f686b0dd09977f558ea0d2665c8582ca
johnny.he27 <johnny.he@titansoft.com.sg>
2023-01-27 23:43:37 +0800

    do leetcode

---
+++ LeetcodeTest/LeetcodeTest946.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Core.Internal;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCodeTest
+{
+    [TestFixture]
+    public class LeetcodeTest946
+    {
+        [Test]
+        public void METHOD()
+        {
+            var leetCode946 = new LeetCode946();
+            var a = leetCode946.ValidateStackSequences(
+                new int[5] { 1, 2, 3, 4, 5 },
+                new int[5] { 4, 3, 5, 1, 2 });
+            a.Should().BeFalse();
+        }
+        [Test]
+        public void METHOD2()
+        {
+            //pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+
+
+            var leetCode946 = new LeetCode946();
+            var a = leetCode946.ValidateStackSequences(
+                new int[5] { 4, 0, 1, 2, 3 },
+                new int[5] { 4, 2, 3, 0, 1 });
+            a.Should().BeFalse();
+        }
+       
+    }
+
+    public class LeetCode946
+    {
+        public bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            var stack = new Stack<int>();
+            stack.Push(pushed[0]);
+            var j = 0;
+            var i= 1;
+            while (j < popped.Length)
+            {
+                //need pop or not
+                //push
+                if (stack.Count !=0 && stack.Peek() == popped[j])
+                {
+                    stack.Pop();
+                    j++;
+                }
+                else
+                {
+                    if (i >= pushed.Length)
+                    {
+                        return false;
+                    }
+                    stack.Push(pushed[i]);
+                    i++;
+                }
+            }
+
+            return true;
+        }
+
+        public bool ValidateStackSequences2(int[] pushed, int[] popped)
+        {
+            var stack = new List<int>()
+            {
+                pushed[0]
+            };
+            var stackIndex = 0;
+            var j = 0;
+            var i = 1;
+            while (j < popped.Length)
+            {
+                //need pop or not
+                //push
+                if (stackIndex != -1 && stack[stackIndex] == popped[j])
+                {
+                    stack.Remove(stack[stackIndex]);
+                    stackIndex--;
+                    j++;
+                }
+                else
+                {
+                    if (i >= pushed.Length)
+                    {
+                        return false;
+                    }
+                    stack.Add(pushed[i]);
+                    i++;
+                    stackIndex++;
+                }
+
+               
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetcodeTest/LeetcodeTest990.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCodeTest
+{
+    [TestFixture]
+    public class LeetcodeTest990
+    {
+        [Test]
+        public void test()
+        {
+            var list = new List<string>()
+            {
+                "b==a",
+                "a==b"
+            };
+            var equationsPossible = new LeetCode990().EquationsPossible(list.ToArray());
+            equationsPossible.Should().BeTrue();
+        }
+
+        [Test]
+        public void test2()
+        {
+            var list = new List<string>()
+            {
+                "c==c",
+                "b==d",
+                "x!=z"
+            };
+            var equationsPossible = new LeetCode990().EquationsPossible(list.ToArray());
+            equationsPossible.Should().BeTrue();
+        }
+
+        [Test]
+        public void test3()
+        {
+            var list = new List<string>()
+            {
+                "a==a",
+            };
+            var equationsPossible = new LeetCode990().EquationsPossible(list.ToArray());
+            equationsPossible.Should().BeTrue();
+        }
+
+        [Test]
+        public void test4()
+        {
+            var list = new List<string>()
+            {
+                "a!=a",
+            };
+            var equationsPossible = new LeetCode990().EquationsPossible(list.ToArray());
+            equationsPossible.Should().BeFalse();
+        }
+
+        [Test]
+        public void test5()
+        {
+            var list = new List<string>()
+            {
+                "c==c",
+                "f!=a",
+                "f==b",
+                "b==c",
+            };
+            var equationsPossible = new LeetCode990().EquationsPossible(list.ToArray());
+            equationsPossible.Should().BeTrue();
+        }
+        [Test]
+        public void test6()
+        {
+            var list = new List<string>()
+            {
+                "a==b",
+                "e==c",
+                "b==c",
+                "a!=e",
+            };
+            
+            var equationsPossible = new LeetCode990().EquationsPossible(list.ToArray());
+            equationsPossible.Should().BeFalse();
+        }
+    }
+    
+
+    public class LeetCode990
+    {
+        public bool EquationsPossible(string[] equations)
+        {
+            var uf = Enumerable.Range(0, 27).ToArray();
+            var eqs = equations.Where(x => x[1] == '=').Select(x => (x[0]-96, x[3]-96));
+            var neqs = equations.Where(x => x[1] == '!').Select(x => (x[0]-96, x[3]-96));
+
+            foreach (var (a, b) in eqs)
+            {
+                uf[GetRoot(a)] = GetRoot(b);
+            }
+
+            foreach (var (a, b) in neqs)
+            {
+                if (GetRoot(a) == GetRoot(b)) return false;
+            }
+
+            return true;
+       
+            // "a==b",
+            // "e==c",
+            // "b==c",
+            // "a!=e",
+            int GetRoot(int i)
+            {
+                while(uf[i] != i)
+                {
+                    uf[i] = uf[uf[i]]; //目前的被assign
+                    i = uf[i];
+                }
+		
+                return i;
+            }            
+        }
+    }
+}
